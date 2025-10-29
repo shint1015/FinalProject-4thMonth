5481e93c7e37cfd6b6869f5311a5384eacca29cf
Shintaro Miyata <shintaroot10@gmail.com>
2025-10-29 16:14:30 -0700

    fix: update JSX attributes to use single quotes and modify text in App component

--- elephant-samurai/src/App.jsx
+++ elephant-samurai/src/App.jsx
@@ -8,26 +8,22 @@ function App() {
 
   return (
     <>
-      <div class="flex">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div class='flex'>
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Eeeedit <code>src/App.jsx</code> and save to test HMRRRRR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
