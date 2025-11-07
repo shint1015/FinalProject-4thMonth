59fd43101e2807bb0bca6876415e397be2236627
Shintaro Miyata <shintaroot10@gmail.com>
2025-11-06 17:01:43 -0800

    fix design

--- elephant-samurai/src/components/layout/Header.jsx
+++ elephant-samurai/src/components/layout/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from '@tanstack/react-router'
 import { useState } from 'react'
 import logo from '@/assets/img/Logo.svg'
 import hamburgerMenuIcon from '@/assets/icon/HamburgerMenu.svg'
+import xmarkIcon from '@/assets/icon/xmark.svg'
+
 export default function Header() {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(undefined)
     const toggleMobileMenu = () => {
@@ -10,12 +12,12 @@ export default function Header() {
 
     return (
         <header className='flex items-center justify-between py-2 px-4 font-extralight'>
-            <div className='flex items-center justify-between w-[60%] gap-8'>
+            <div className='flex items-center justify-between w-[60%] lg:w-[45%] xl:w-[33%]'>
                 <div className='logo-container'>
                     <img src={logo} alt='Elephant Samurai Logo' className='h-7 sm:h-14' />
                 </div>
                 <nav className='sm:block hidden w-[80%]'>
-                    <ul className='flex space-x-8'>
+                    <ul className='flex space-x-8 text-center'>
                         <li>
                             <Link to='/' className='text-primary-white hover:text-primary-gray'>
                                 Home
@@ -50,22 +52,26 @@ export default function Header() {
                     Log in
                 </button>
                 <button id='hamburger-menu' className='sm:hidden block' onClick={toggleMobileMenu}>
-                    <img src={hamburgerMenuIcon} alt='Menu' className='h-7 sm:h-14' />
+                    <img
+                        src={isMobileMenuOpen ? xmarkIcon : hamburgerMenuIcon}
+                        alt='Menu'
+                        className='h-7 sm:h-14'
+                    />
                 </button>
             </div>
             <div
                 id='mobile-menu'
                 className={[
-                    'absolute top-10 left-0 w-full bg-primary-black px-4 py-4 sm:hidden',
-                    isMobileMenuOpen == undefined
+                    'absolute top-10 left-0 w-full bg-primary-black px-4 py-4 sm:hidden z-9999 h-full',
+                    isMobileMenuOpen === undefined
                         ? 'hidden'
                         : isMobileMenuOpen
-                          ? 'animate-slide-in-right'
-                          : 'animate-slide-out-right',
+                          ? 'animate-slide-in-top'
+                          : 'animate-slide-out-top',
                 ].join(' ')}
             >
-                <ul className='flex flex-col space-y-4 items-end'>
-                    <li>
+                <ul className='flex flex-col space-y-4 items-center'>
+                    <li className=''>
                         <Link to='/' className='text-primary-white hover:text-primary-gray'>
                             Home
                         </Link>
@@ -76,7 +82,7 @@ export default function Header() {
                         </Link>
                     </li>
                     <li>
-                        <Link to='/contact' className='text-primary-white hover:text-primary-gray'>
+                        <Link to='/about' className='text-primary-white hover:text-primary-gray'>
                             About
                         </Link>
                     </li>
