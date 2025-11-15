4eadcf4dd0252984ccabd0dc8c31ec01b9f96960
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-11-14 17:04:26 -0800

    Fix Route
    Right now The Profile is just layout header and move info profile to profile form page

--- elephant-samurai/src/route.jsx
+++ elephant-samurai/src/route.jsx
@@ -77,6 +77,24 @@ export const showDetailRoute = createRoute({
     getParentRoute: () => rootRoute,
     path: '/show/$showId',
     component: () => <Detail />,
+});
+
+export const profileRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: '/profile',
+    component: () => <Profile />,
+});
+
+export const profileFormRoute = createRoute({
+    getParentRoute: () => profileRoute,
+    path: 'info',
+    component: () => <ProfileForm />,
+});
+
+export const myTicketsRoute = createRoute({
+  getParentRoute: () => profileRoute,
+  path: 'mytickets',
+  component: () => <MyTickets />,
 })
 
 export const profileRoute = createRoute({
