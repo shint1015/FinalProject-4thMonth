5359051fb79b9b5987a309c9ac65c53ecbf14f2d
Aiya Tossapol <aiya.tossapol@gmail.com>
2025-11-17 12:40:56 -0800

    Fix innerapp

--- elephant-samurai/src/components/layout/InnerApp.jsx
+++ elephant-samurai/src/components/layout/InnerApp.jsx
@@ -1,10 +1,25 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { useAuth } from '@/hook/useAuth'
+import { useEffect, useRef } from 'react'
 import router from '@/route'
 
 const InnerApp = () => {
     const auth = useAuth()
+
+    const authRef = useRef(auth)
+    useEffect(() => {
+        authRef.current = auth
+    }, [auth])
+
+    if (auth.isLoading) {
+        return (
+            <div className='min-h-screen flex items-center justify-center text-primary-white'>
+                Loading...
+            </div>
+        )
+    }
+
     return (
         <>
             <RouterProvider router={router} context={{ auth }} />
