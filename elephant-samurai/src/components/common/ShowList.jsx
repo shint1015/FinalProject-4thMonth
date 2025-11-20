f4a23775a108a5894b4a71da5f5fa892f3d9a019
Shintaro Miyata <shintaroot10@gmail.com>
2025-11-19 19:05:30 -0800

    fix

--- elephant-samurai/src/components/common/ShowList.jsx
+++ elephant-samurai/src/components/common/ShowList.jsx
@@ -2,14 +2,14 @@ import ShowCard from './ShowCard'
 
 export default function ShowList({ shows }) {
     return (
-        <section className="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:w-5/6 mx-auto">
+        <section className='grid grid-cols-2 lg:grid-cols-4 gap-8 lg:w-5/6 mx-auto'>
             {shows.length > 0 ? (
-                shows.map((show) => <ShowCard key={show.id} show={show} />)
+                shows.map(show => <ShowCard key={show.id} show={show} />)
             ) : (
-                <p className="col-span-full text-center text-primary-yellow text-detail">
+                <p className='col-span-full text-center text-primary-yellow text-detail'>
                     No shows found.
                 </p>
             )}
         </section>
-    );
+    )
 }
--- elephant-samurai/src/pages/Home.jsx
+++ elephant-samurai/src/pages/Home.jsx
@@ -1,86 +1,100 @@
-import HeroSectionVideo from "@/assets/video/HeroSection.webm"
-import Location from '@/assets/img/Location.png';
+import HeroSectionVideo from '@/assets/video/HeroSection.webm'
+import Location from '@/assets/img/Location.png'
 import ShowList from '@/components/common/ShowList'
-import { useNavigate } from '@tanstack/react-router';
+import { useNavigate } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
 
 function LocationPhoto() {
-  return <img src={Location} alt="Location" className="lg:w-[50%]"/>;
+    return <img src={Location} alt='Location' className='lg:w-[50%]' />
 }
 
 export default function Home() {
-    const navigate = useNavigate();
+    const [shows, setShows] = useState([])
+    const navigate = useNavigate()
     const HandleSeeAll = () => {
-      navigate({ to: "/shows" });
+        navigate({ to: '/shows' })
     }
     const HandleSeat = () => {
-      navigate({ to: "/about#seatinfo" });
+        navigate({ to: '/about#seatinfo' })
     }
+
+    useEffect(() => {
+        fetch('/data/event.json')
+            .then(res => res.json())
+            .then(setShows)
+            .catch(err => console.error('Error fetching shows:', err))
+    }, [])
     return (
         <>
             {/* Hero section */}
-          <div className="relative w-full overflow-hidden">
-            <video src={HeroSectionVideo} className="absolute  top-0 left-0 w-full h-full object-cover" autoPlay loop muted playsInline/>
-            <div className="relative w-[100dvw] h-[60dvh] sm:h-[60dvh] lg:h-[100dvh] flex flex-col items-center justify-center text-center">
-              <h1 className="text-primary-yellow text-display">
-                SAMURAI
-              </h1>
-              <h1 className="mt-[-1rem] text-primary-yellow text-display">
-                ELEPHANT
-              </h1>
-              <p className="text-primary-yellow text-title">
-                Studio Theatre
-              </p>
+            <div className='relative w-full overflow-hidden'>
+                <video
+                    src={HeroSectionVideo}
+                    className='absolute  top-0 left-0 w-full h-full object-cover'
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                />
+                <div className='relative w-[100dvw] h-[60dvh] sm:h-[60dvh] lg:h-[100dvh] flex flex-col items-center justify-center text-center'>
+                    <h1 className='text-primary-yellow text-display'>SAMURAI</h1>
+                    <h1 className='mt-[-1rem] text-primary-yellow text-display'>ELEPHANT</h1>
+                    <p className='text-primary-yellow text-title'>Studio Theatre</p>
+                </div>
             </div>
-          </div>
 
-          {/* Upcoming event */}
-          <div className="p-[2rem] sm:p-[3rem] lg:p-[5rem]">
-            <h1 className="text-primary-yellow text-h1">
-              UPCOMING EVENT
-            </h1>
-            <ShowList/>
-            <div className="text-center">
-              <button onClick={HandleSeeAll} 
-                className='border border-primary-yellow text-body text-primary-yellow font-dm-sans px-6 py-2 rounded-md hover:bg-primary-yellow hover:text-black'>
-                See All Event
-              </button>
+            {/* Upcoming event */}
+            <div className='p-[2rem] sm:p-[3rem] lg:p-[5rem]'>
+                <h1 className='text-primary-yellow text-h1'>UPCOMING EVENT</h1>
+                <ShowList shows={shows} />
+                <div className='text-center'>
+                    <button
+                        onClick={HandleSeeAll}
+                        className='border border-primary-yellow text-body text-primary-yellow font-dm-sans px-6 py-2 rounded-md hover:bg-primary-yellow hover:text-black'
+                    >
+                        See All Event
+                    </button>
+                </div>
             </div>
-          </div>
 
-          {/* History */}
-          <div className="relative w-full h-[550px] bg-cover bg-[image:var(--bg-history)] flex justify-center">
-            <div className="p-[5rem] flex justify-center flex-col text-center lg:w-[55%]">
-              <h4 className="text-primary-yellow text-h4">
-                SINCE 1995,
-              </h4>
-              <p className="mt-4 text-primary-white text-body">
-                For more than twenty years, Samurai Elephant has stood as a space where creativity meets passion, Uniting artists and audiences through unforgettable stories on stage.
-              </p>
-              <p className="mt-4 text-primary-white text-body">
-                Explore our <a className="underline cursor-pointer" onClick={HandleSeat}>seating and venue layout ↗</a> to plan your perfect view.
-              </p>
+            {/* History */}
+            <div className='relative w-full h-[550px] bg-cover bg-[image:var(--bg-history)] flex justify-center'>
+                <div className='p-[5rem] flex justify-center flex-col text-center lg:w-[55%]'>
+                    <h4 className='text-primary-yellow text-h4'>SINCE 1995,</h4>
+                    <p className='mt-4 text-primary-white text-body'>
+                        For more than twenty years, Samurai Elephant has stood as a space where
+                        creativity meets passion, Uniting artists and audiences through
+                        unforgettable stories on stage.
+                    </p>
+                    <p className='mt-4 text-primary-white text-body'>
+                        Explore our{' '}
+                        <a className='underline cursor-pointer' onClick={HandleSeat}>
+                            seating and venue layout ↗
+                        </a>{' '}
+                        to plan your perfect view.
+                    </p>
+                </div>
             </div>
-          </div>
 
-
-          {/* Location */}
-           <div className="p-[2rem] sm:p-[3rem] lg:p-[5rem]">
-            <h1 className="text-primary-yellow text-h1">
-                FIND US HERE
-            </h1>
-            <div className="mt-4 flex flex-col sm:flex-col lg:flex-row gap-[2rem] sm:gap-[3rem] lg:gap-[5rem] justify-center items-center">
-              <LocationPhoto/>
-              <div>
-                <p className="text-primary-white text-body">
-                  The Sumarai Elephant Studio theater is located in the heart of downtown Vancouver at 111 Hollywood Street.<br></br>
-                  There are several parking lots within walking distance from the theatre in addition to meter parking on Daiki St. & Shin St.
-                </p>
-                <p className="mt-4 text-primary-white text-body">
-                  Parking lot information, Easy Park: <a className="underline">www.easypark.ca ↗</a>
-                </p>
-              </div>
+            {/* Location */}
+            <div className='p-[2rem] sm:p-[3rem] lg:p-[5rem]'>
+                <h1 className='text-primary-yellow text-h1'>FIND US HERE</h1>
+                <div className='mt-4 flex flex-col sm:flex-col lg:flex-row gap-[2rem] sm:gap-[3rem] lg:gap-[5rem] justify-center items-center'>
+                    <LocationPhoto />
+                    <div>
+                        <p className='text-primary-white text-body'>
+                            The Sumarai Elephant Studio theater is located in the heart of downtown
+                            Vancouver at 111 Hollywood Street.<br></br>
+                            There are several parking lots within walking distance from the theatre
+                            in addition to meter parking on Daiki St. & Shin St.
+                        </p>
+                        <p className='mt-4 text-primary-white text-body'>
+                            Parking lot information, Easy Park:{' '}
+                            <a className='underline'>www.easypark.ca ↗</a>
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
         </>
     )
 }
